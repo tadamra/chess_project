d558ae8d62825316b0d65f2f732ba39c6c8a6d3e
nlsgrndn <nils.grandien@gmail.com>
2023-08-15 17:11:44 +0200

    added option play against AI via GUI

---
+++ chess_playing_GUI.py
@@ -0,0 +1,89 @@
+#IMPORTS
+import numpy as np
+import chess
+import torch
+import torch.nn.functional as F
+import chess.svg
+import tkinter as tk
+from PIL import Image, ImageTk
+import cairosvg
+import io
+from util import load_model, choose_move
+
+# SPECIFY DEFAULT SETTINGS
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+class ChessGUI:
+    def __init__(self, root, ai_move_function):
+        self.root = root
+        self.root.title("Chess Game")
+
+        self.board = chess.Board()
+        self.ai_move_function = ai_move_function
+
+        self.model = load_model()
+
+        self.canvas = tk.Canvas(root, width=400, height=400)
+        self.canvas.pack()
+
+        self.update_board()
+
+        self.root.bind("<Button-1>", self.on_board_click)
+        self.state = 0
+        self.from_square = None
+        self.to_square = None
+
+
+    def update_board(self):
+        board_svg = chess.svg.board(board=self.board)
+        img_bytes = cairosvg.svg2png(bytestring=board_svg)
+        img = Image.open(io.BytesIO(img_bytes))
+        self.tk_img = ImageTk.PhotoImage(img)
+        self.canvas.create_image(0, 0, anchor="nw", image=self.tk_img)
+
+
+    def on_board_click(self, event):
+        if not self.board.is_game_over():
+            if self.state == 0:
+                col = event.x // 50
+                row = 7 - event.y // 50
+                self.from_square = chess.square(col, row)
+                self.state = 1
+            elif self.state == 1:
+                col = event.x // 50
+                row = 7 - event.y // 50
+                self.to_square = chess.square(col, row)
+                legal_moves = [move for move in self.board.legal_moves if move.from_square == self.from_square and move.to_square == self.to_square]
+                if len(legal_moves) > 0:
+                    move = legal_moves[0]  # You can implement a way for the player to select the move
+                    self.board.push(move)
+                    self.update_board()
+                    self.state = 2
+                    print("Player moved:", move)
+                    print("state:", self.state)
+                else:
+                    self.state = 0
+            elif self.state == 2:
+                print("AI is thinking...")
+                if not self.board.is_game_over() and self.board.turn == chess.BLACK:
+                    ai_move = self.ai_move_function(self.model, self.board, chess.BLACK)
+                    self.board.push(ai_move)
+                    self.update_board()
+                self.state = 0
+            else:
+                print("Invalid state")
+            if self.board.is_game_over():
+                print("Game Over")
+                print("Result:", self.board.result())
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    chess_gui = ChessGUI(root, choose_move)
+    root.mainloop()
+
+
+
+
+    
+
---
+++ neural_networks.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class CopiedSubModule(nn.Module):
+    
+    def __init__(self, hidden_size):
+        super(CopiedSubModule, self).__init__()
+        self.conv1 = nn.Conv2d(hidden_size, hidden_size, 3, padding = 1)
+        self.conv2 = nn.Conv2d(hidden_size, hidden_size, 3, padding = 1)
+        self.bn1 = nn.BatchNorm2d(hidden_size)
+        self.bn2 = nn.BatchNorm2d(hidden_size)
+        self.activation1 = nn.SELU()
+        self.activation2 = nn.SELU()
+
+    def forward(self, x):
+        x_input = x.clone()
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x += x_input
+        x = self.activation2(x)
+        return x
+    
+class CopiedChessNetwork(nn.Module):
+
+    def __init__(self, hidden_layers, hidden_size):
+        super(CopiedChessNetwork, self).__init__()
+        self.hidden_layers = hidden_layers
+        self.input_layer = nn.Conv2d(6, hidden_size, 3, stride = 1, padding = 1)
+        self.module_list = nn.ModuleList([CopiedSubModule(hidden_size) for _ in range(hidden_layers)])
+        self.output_layer = nn.Conv2d(hidden_size, 2, 3, stride = 1, padding = 1)
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = F.relu(x)
+
+        for i in range(self.hidden_layers):
+            x = self.module_list[i](x)
+
+        x = self.output_layer(x)
+        return x
