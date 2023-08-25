f4bbcb5aa2f30d927b07c59c1ebdee6c5c36fc6f
Kévin-Lâm Quesnel <kevin-lam@hotmail.fr>
2023-08-25 12:38:11 +0200

    keep implementing CNN for position evaluation

--- positionEvaluation/EvaluationCNN.py
+++ positionEvaluation/EvaluationCNN.py
@@ -1,23 +1,16 @@
-import numpy as np
-import pandas as pd
-import chess
-import gc
-import re
-import torch
-from torch.utils.data import DataLoader, Dataset
 import torch.nn as nn
 import torch.nn.functional as F
-from processGame import board_to_rep
 
-num_to_letter = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5:'f', 6:'g', 7:'h'}
+num_to_letter = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
 letter_to_num = {v: k for k, v in num_to_letter.items()}
+
 
 class CopiedSubModule(nn.Module):
 
     def __init__(self, hidden_size):
         super(CopiedSubModule, self).__init__()
-        self.conv1 = nn.Conv2d(hidden_size, hidden_size, 3, padding = 1)
-        self.conv2 = nn.Conv2d(hidden_size, hidden_size, 3, padding = 1)
+        self.conv1 = nn.Conv2d(hidden_size, hidden_size, 3, padding=1)
+        self.conv2 = nn.Conv2d(hidden_size, hidden_size, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(hidden_size)
         self.bn2 = nn.BatchNorm2d(hidden_size)
         self.activation1 = nn.SELU()
@@ -34,22 +27,29 @@ class CopiedSubModule(nn.Module):
         x = self.activation2(x)
         return x
 
+
 class CopiedChessNetwork(nn.Module):
 
     def __init__(self, hidden_layers, hidden_size):
         super(CopiedChessNetwork, self).__init__()
         self.hidden_layers = hidden_layers
-        self.input_layer = nn.Conv2d(6, hidden_size, 3, stride = 1, padding = 1)
-        self.linear1 = nn.Linear(hidden_size, 128)
+        self.input_layer = nn.Conv2d(6, hidden_size, 3, stride=1, padding=1)
+        self.linear1 = nn.Linear(8*8*hidden_size, 128)
+        self.linear2 = nn.Linear(128, 64)
         self.module_list = nn.ModuleList([CopiedSubModule(hidden_size) for _ in range(hidden_layers)])
-        self.output_layer = nn.Linear(128,1)
+        self.output_layer = nn.Linear(64, 1)
+
     def forward(self, x):
         x = self.input_layer(x)
         x = F.relu(x)
+
         for i in range(self.hidden_layers):
             x = self.module_list[i](x)
-        x = x.view(x.size(0),)
-        x = self.linear1()
+        # Flatten the tensor for the linear layers
+        x = x.view(x.size(0), -1)  # Reshape to (batch_size, hidden_size * output_height * output_width)
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
         x = F.relu(x)
         x = self.output_layer(x)
         return x
--- positionEvaluation/processGame.py
+++ positionEvaluation/processGame.py
@@ -1,5 +1,7 @@
 import chess
 import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 num_to_letter = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
 letter_to_num = {v: k for k, v in num_to_letter.items()}
@@ -16,3 +18,39 @@ def board_to_rep(board: chess.Board):
         i = pieces.index(piece.symbol().lower())
         layers[i, row, column] = 1 if piece.symbol().isupper() else -1  # 1 for white, -1 for black
     return layers
+
+
+class Chess_Dataset_white(Dataset):
+    def __init__(self, positions, targets):
+        self.targets = targets
+        self.positions = positions
+
+    def __len__(self):
+        return np.shape(self.targets)[0]
+
+    def __getitem__(self, idx):
+        index = idx
+        while abs(self.targets[index, 0]) > 999000: # exclude the mates
+            index -= 1
+        if self.targets[index, 1]:
+            target = self.targets[index, 0]
+            position = self.positions[index, :, :, :]
+
+        elif index > 0:
+            target = self.targets[index - 1, 0]
+            position = self.positions[index - 1, :, :, :]
+        else:
+            target = self.targets[index + 1, 0]
+            position = self.positions[index + 1, :, :, :]
+        sample = {"position": position, "target": target}
+        return sample
+
+
+def collate_batch(batch):
+    return torch.tensor([item['target'] for item in batch]), torch.stack(
+        [torch.tensor(item['position']) for item in batch])
+
+
+def get_samples_white(input, labels, batch_size):
+    dataset = Chess_Dataset_white(input, labels)
+    return DataLoader(dataset, batch_size=batch_size, collate_fn=collate_batch)
